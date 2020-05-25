f67b51cc2f7b048328e81b4642c9614abccc13f3
juhyeok94 <perfect3486@gmail.com>
2020-05-25 18:19:25 +0900

    2020-05-25-v2

--- src/java_20200518/CalendarDemo.java
+++ src/java_20200518/CalendarDemo.java
@@ -4,7 +4,7 @@ public class CalendarDemo {
 	public static void main(String[] args) {
 		Calendar c = new Calendar();
 		c.set(2020, 5, 25);
-		c.setYear(2025);//Calendat 클래스에서 변경할수 있게 만듬
+		c.setYear(2025);//Calendar 클래스에서 변경할수 있게 만듬
 		c.print();
 	}
 }
