e58d8a84c23e2f28657bb0646d8ece07e5640aee
juhyeok94 <perfect3486@gmail.com>
2020-06-02 17:04:35 +0900

    2020-06-02-v1

---
+++ src/java_20200602/JdbcDemo.java
@@ -0,0 +1,62 @@
+package java_20200602;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JdbcDemo {
+	public static void main(String[] args) {
+		
+		//1. 드라이브 로드한다.
+		//Driver 객체를 메모리에 로드하여 Java Application과 mariadb-java-Client-xxx.jar과 연결한다.
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			System.out.println("드라이버 로드 성공");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.err.println("드라이버 로드 실패");
+		}
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			//2. 데이터 베이스와 연결
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/kpc",//url
+					"kpc12",//user
+					"kpc1234");//password
+			
+			//3. SQL문을 전송할 수 있는 PreparedStatement 객체를 생성
+			//?의 의미는 바인딩 변수 : 바인딩 변수는 반드시 컬럼 값에만 사용해야 한다.
+			StringBuffer sql = new StringBuffer();
+			sql.append("INSERT INTO member1(num, NAME, addr) VALUES(?,?,?)");
+			pstmt = con.prepareStatement(sql.toString());
+			
+			//4. 바인딩 변수 설정과 쿼리 전송
+			pstmt.setInt(1, 12);
+			pstmt.setString(2, "샤넬");
+			pstmt.setString(3, "프랑스");
+	
+			//5. SQL문 전송
+			int resultCount = pstmt.executeUpdate();
+			System.out.println("갱신된 행의수 : "+resultCount);
+			
+			System.out.println("데이터 베이스(Mariadb) 연결 성공.....");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			//6. 모든 자원을 반납한다.
+			try {
+				if(con != null) con.close();
+				if(pstmt != null) pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	
+	
+	}
+}
---
+++ src/java_20200602/JdbcDemo2.java
@@ -0,0 +1,61 @@
+package java_20200602;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JdbcDemo2 {
+	public static void main(String[] args) {
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			System.out.println("드라이버 로드 성공");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.err.println("드라이버 로드 실패");
+		}
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/kpc",
+					"kpc12",
+					"kpc1234");
+			StringBuffer sql = new StringBuffer();
+			sql.append("INSERT INTO member1(num, NAME, addr) VALUES(?,?,?)");
+			pstmt = con.prepareStatement(sql.toString());
+			
+
+			pstmt.setInt(1, 12);
+			pstmt.setString(2, "샤넬");
+			pstmt.setString(3, "프랑스");
+	
+			int resultCount = pstmt.executeUpdate();
+			System.out.println("갱신된 행의수 : "+resultCount);
+			
+			System.out.println("데이터 베이스(Mariadb) 연결성공");
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(con != null)
+				try {
+					if(con != null) con.close();
+					if(pstmt != null) pstmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+		
+		
+		
+		
+		
+	}
+}
